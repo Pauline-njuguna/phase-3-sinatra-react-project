7109e1e8bbfb185329c7f5b0332343e25734861a
pauline-njunguna <paulinenjunguna.w@gmail.com>
2023-03-06 07:33:39 +0300

    created a faker seed file to run the samples

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
-puts "🌱 Seeding spices..."
+ActiveRecord::Base.connection.execute('PRAGMA foreign_keys = OFF')
 
-# Seed your database here
+require 'faker'
 
-puts "✅ Done seeding!"
+
+# Create recipes
+50.times do
+  Recipe.create(
+    image_url: Faker::LoremFlickr.image,
+    Meal: Faker::Food.dish,
+    Category: Faker::Restaurant.type,
+    Area: Faker::Nation.nationality,
+    instructions: Faker::Food.description,
+    Ingredient: Faker::Food.ingredient
+  )
+end
+
+# Create reviews
+50.times do
+  Review.create(
+    content: Faker::Lorem.paragraphs(number: 2).join("\n"),
+    rating: Faker::Number.between(from: 1, to: 5),
+    recipe_id: Faker::Number.between(from: 1, to: 50),
+  )
+end
+
+ActiveRecord::Base.connection.execute('PRAGMA foreign_keys = ON')
